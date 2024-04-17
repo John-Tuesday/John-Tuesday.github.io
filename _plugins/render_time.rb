d61d3d3059318ef75c19bcf77280d6f5be2a8aeb
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-04-17 02:51:09 -0500

    Create initial default style liquid plugin
    Disables default stylesheets front matter.
    The plugin should supercede this.
    
    Modified custom-plugin.html for testing purposes

--- _plugins/render_time.rb
+++ _plugins/render_time.rb
@@ -32,13 +32,58 @@ module Jekyll
     def initialize(tag_name, path, tokens)
       super
       @path = path.to_s.strip!
+      @head_key = 'head'
+      @key = 'stylesheets'
     end
 
     def render(context)
+      @context = context
+      page = @context.registers[:page]
+      page[@head_key] ||= {}
+      page[@head_key][@key] ||= []
+      page[@head_key][@key] << @path
+    end
+  end
+
+  def if_file_exists(site, path)
+    site.static_files.each do |file|
+      return file if file.url == path
+    end
+
+    return false
+  end
+
+  class DefaultStylesheetGen < Jekyll::Generator
+    include Jekyll
+
+    @@default_path = 'style.css'
+
+    def initialize(config)
       super
-      sheets = context["head.stylesheets"] || []
-      sheets << @path
-      context["head.stylesheets"] = sheets
+      @head_key ||= 'head'
+      @stylesheets_key ||= 'stylesheets'
+    end
+
+    # if stylesheet is not specified and default exists, use default
+    #   default is `style.css` relative to each page
+    def generate(site)
+      site.pages.each do |pg|
+        pg.data[@head_key]
+
+        # check if already defined
+        if pg.data[@head_key] && !pg.data[@head_key][@stylesheets_key]&.empty?
+          return
+        end
+
+        # check default
+        default_style = PathManager.join(pg.dir, @@default_path)
+        exists = !!if_file_exists(site, default_style)
+        if exists
+          # add_default
+          pg.data[@head_key] ||= {}
+          pg.data[@head_key][@stylesheets_key] = [default_style]
+        end
+      end
     end
   end
 end
