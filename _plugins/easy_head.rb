026f4977edfecefa90b407a0db19ff6f64bf0345
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-04-17 22:45:56 -0500

    Add validation for add_stylesheet

--- _plugins/easy_head.rb
+++ _plugins/easy_head.rb
@@ -1,4 +1,42 @@
 module EasyHead
+  module Errors
+    Error = Class.new(::RuntimeError)
+
+    FileDoesNotExist = Class.new(Error)
+  end
+
+  module Verify
+    module_function
+
+    def is_relative_path?(path)
+      !is_absolute_path?(path)
+    end
+
+    def is_absolute_path?(path)
+      path.start_with?('/')
+    end
+
+    def find_file(page, site, path, on_not_found = nil)
+      resolved_path = 
+        if is_absolute_path?(path) 
+          path
+        else 
+          Jekyll::PathManager.join(page['dir'], path)
+        end
+      file = [site.static_files, site.pages]
+        .find { |files| files.find { |elem| elem.url == resolved_path } }
+      if !file
+        return on_not_found.call(resolved_path) if on_not_found
+        raise Errors::FileDoesNotExist, <<~FILE_DNE
+          File does not exist! (url: #{page['url']})
+            input: #{path}
+            relative to: #{page['dir']}
+            resolved path: #{resolved_path} 
+        FILE_DNE
+      end
+    end
+  end
+
   class AddStylesheetTag < Liquid::Tag
     LiquidName = 'add_stylesheet'
 
@@ -7,11 +45,14 @@ module EasyHead
       @path = path.to_s.strip!.undump
       @head_key = 'head'
       @key = 'stylesheets'
+      @line_number = tokens.line_number
     end
 
     def render(context)
       @context = context
       page = @context.registers[:page]
+      site = @context.registers[:site]
+      file = Verify::find_file(page, site, @path)
       page[@head_key] ||= {}
       styles = page[@head_key][@key] ||= []
       styles << @path unless styles.include?(@path)
